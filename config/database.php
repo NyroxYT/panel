a4b1a6a9be9cdc0dd9d3c91b113dac2e1c59698d
Nyrox <smtp.nypay@gmail.com>
2026-09-12 17:12:40 +0400

    config(panel): add SQLite connection as primary database

--- config/database.php
+++ config/database.php
@@ -4,37 +4,21 @@ use Illuminate\Support\Str;
 use Pterodactyl\Helpers\Time;
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Default Database Connection Name
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for database operations. This is
-    | the connection which will be utilized unless another connection
-    | is explicitly specified when you execute a query / statement.
-    |
-    */
-
-    'default' => env('DB_CONNECTION', 'mysql'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here are each of the database connections setup for your application.
-    | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
-    |
-    |
-    | All database work in Laravel is done through the PHP PDO facilities
-    | so make sure you have the driver for your particular database of
-    | choice installed on your machine before you begin development.
-    |
-    */
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     'connections' => [
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DB_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => env('DB_PREFIX', ''),
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            'busy_timeout' => 5000,
+            'journal_mode' => 'WAL',
+            'synchronous' => 'NORMAL',
+        ],
+
+        // Retained for compatibility with existing deployments and migrations.
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL', env('DATABASE_URL')),
@@ -48,7 +32,7 @@ return [
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
             'prefix' => env('DB_PREFIX', ''),
             'prefix_indexes' => true,
-            'strict' => env('DB_STRICT_MODE', false), // TODO: true by default
+            'strict' => env('DB_STRICT_MODE', false),
             'engine' => null,
             'timezone' => env('DB_TIMEZONE', Time::getMySQLTimezoneOffset(env('APP_TIMEZONE', 'UTC'))),
             'sslmode' => env('DB_SSLMODE', 'prefer'),
@@ -86,41 +70,17 @@ return [
         ],
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run on the database.
-    |
-    */
-
     'migrations' => [
         'table' => 'migrations',
         'update_date_on_publish' => true,
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as Memcached. You may define your connection settings here.
-    |
-    */
-
     'redis' => [
         'client' => env('REDIS_CLIENT', 'predis'),
-
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'pterodactyl'), '_') . '_database_'),
+            'prefix' => Str::slug(env('APP_NAME', 'Nx Panel'), '_') . '_database_',
         ],
-
         'default' => [
             'scheme' => env('REDIS_SCHEME', 'tcp'),
             'path' => env('REDIS_PATH', '/run/redis/redis.sock'),
@@ -129,17 +89,7 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DATABASE', 0),
-            'context' => extension_loaded('redis') && env('REDIS_CLIENT') === 'phpredis' ? [
-                'stream' => array_filter([
-                    'verify_peer' => env('REDIS_VERIFY_PEER', true),
-                    'verify_peer_name' => env('REDIS_VERIFY_PEER_NAME', true),
-                    'cafile' => env('REDIS_CAFILE'),
-                    'local_cert' => env('REDIS_LOCAL_CERT'),
-                    'local_pk' => env('REDIS_LOCAL_PK'),
-                ]),
-            ] : [],
         ],
-
         'sessions' => [
             'scheme' => env('REDIS_SCHEME', 'tcp'),
             'path' => env('REDIS_PATH', '/run/redis/redis.sock'),
@@ -148,15 +98,6 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DATABASE_SESSIONS', 1),
-            'context' => extension_loaded('redis') && env('REDIS_CLIENT') === 'phpredis' ? [
-                'stream' => array_filter([
-                    'verify_peer' => env('REDIS_VERIFY_PEER', true),
-                    'verify_peer_name' => env('REDIS_VERIFY_PEER_NAME', true),
-                    'cafile' => env('REDIS_CAFILE'),
-                    'local_cert' => env('REDIS_LOCAL_CERT'),
-                    'local_pk' => env('REDIS_LOCAL_PK'),
-                ]),
-            ] : [],
         ],
     ],
 ];
